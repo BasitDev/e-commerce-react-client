ba70802703c37fb55de535e60c439d9b299da1d8
Abdul Basit <abhiawan1227@gmail.com>
2022-08-23 15:01:41 +0500

    navbar announcement products and category design added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Announcement from "./../components/Announcement";
+import Slider from "./../components/Slider";
+import Categories from "./../components/Categories";
+import Products from "./../components/Products";
 
 const Home = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />;
+    </>
+  );
 };
 
 export default Home;
